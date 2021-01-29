0d48ee5a02dc7df09bacdf2011a6e612001962d5
Ramesh-Nuwan <rameshnuwan1996@gmail.com>
2021-01-29 10:53:37 +0530

    PHP FILE CHANGED

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    Lorem ipsum, dolor sit amet
+    consectetur adipisicing elit.
+    Beatae ab magni eos, incidunt suscipit hic odio aspernatur
+    magnam sit et illum optio sed sequi, nisi aliquid
+    ratione voluptas consectetur similique!
+</body>
+
+</html>
